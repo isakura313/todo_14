9c1321b271ce36339ac4ac4be61ec5a114a96b33
Paul <isakura313@gmail.com>
2019-12-11 20:43:59 +0300

    cool

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 class ItemDeal{
-    constructor(name,color){
-        this.name = name;
+    constructor(content, color){
+        this.content = content;
         this.color = color;
         this.now = new Date;
     }
@@ -26,5 +26,36 @@ let Animation_Array = [
     "lightSpeedOut"
 ];
 let Month_Array = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября','Ноября', 'Декабря'];
+
+let select = document.querySelector("select");
+let field = document.querySelector("input");
+let button = document.querySelector(".button_plus");
+let deals = document.querySelector(".deals");
+
+//функция создания нашего дела
+function addDeal(){
+    let content = field.value;
+    if(!content){
+        return;
+    }
+    let item = new ItemDeal(content, select.value-1);
+    let item_to_JSON = JSON.stringify(item);
+    localStorage.setItem(+item.now, item_to_JSON);
+    // GenerateDOM(item);
+    field.value = '';
+}
+
+button.addEventListener("click", addDeal);
+
+
+
+
+
+
+
+// самовызывающаяся функция
+// при запуске приложения она отрисовывает все наши дела 
+// из localStorage
+
 
 
