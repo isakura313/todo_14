f8a15fea8a5688e9d00ce1efdfa41e2b1b1aa8a2
Paul <isakura313@gmail.com>
2019-12-11 20:04:43 +0300

    all

---
+++ ItemDeal.js
@@ -0,0 +1,8 @@
+
+class ItemDeal{
+    constructor(name,color){
+        this.name = name;
+        this.color = color;
+        this.now = new Date;
+    }
+}
---
+++ code.js
@@ -0,0 +1,30 @@
+class ItemDeal{
+    constructor(name,color){
+        this.name = name;
+        this.color = color;
+        this.now = new Date;
+    }
+}
+
+let motivation_array = [
+    'люблю webpack',
+    'кто не работает, тот ест',
+    'как не быть ленивым??',
+    'Д/з само себя не сделает))'
+];
+
+let IA = [
+    'has-text-danger',
+    'has-text-warning',
+    'has-text-success'
+];
+
+let Animation_Array = [
+    'bounceOut',
+    'rollOut',
+    'rotateOut',
+    "lightSpeedOut"
+];
+let Month_Array = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября','Ноября', 'Декабря'];
+
+
